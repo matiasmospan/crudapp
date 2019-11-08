e898b138c78c9e7aa58fcc6387854fdc14833fb6
matiasmospan <31488497+matiasmospan@users.noreply.github.com>
2019-11-08 14:35:45 -0300

    Update and rename config.ini to config.php

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_SERVER', 'mysql');
+define('DB_USERNAME', 'phpcrud');
+define('DB_PASSWORD', 'phpcrud');
+define('DB_NAME', 'php_crud');
+$link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if ($link->connect_errno) {
+    printf("Connect failed: %s\n", $link->connect_error);
+    exit();
+}
